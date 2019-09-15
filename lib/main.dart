85e0146f1df1bda33a437d64623791f4ecfd708f
= <=>
2019-09-15 11:59:53 -0300

    First version using sharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,144 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
+import 'models/item.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(App());
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TODO App',
+      debugShowCheckedModeBanner: false, // tira a faixa debug
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
 
-  final String title;
+  HomePage() {
+    items = [];
+
+    /*
+    items.add(Item(title: "To Study Dart", done: true));
+    items.add(Item(title: "To Study JS", done: false));
+    items.add(Item(title: "To Study JAVA and Spring", done: false));
+    items.add(Item(title: "To Stydy NodeJS", done: false));
+    items.add(Item(title: "To Stydy REACTJS", done: false));
+    items.add(Item(title: "To Stydy REACT NATIVE", done: false));
+    */
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  var newTaskController = TextEditingController();
 
-  void _incrementCounter() {
+  _HomePageState() {
+    load();
+  }
+
+  void add() {
+    if (newTaskController.text.isEmpty) return;
+
     setState(() {
-      _counter++;
+      widget.items.add(
+        Item(
+          title: newTaskController.text,
+          done: false,
+        ),
+      );
+      newTaskController.clear();
+      save();
     });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    var preferenceDb = await SharedPreferences.getInstance();
+    var data = preferenceDb.getString('data');
+
+    if (data != null) {
+      Iterable jsonDecoded = jsonDecode(data);
+      List<Item> result = jsonDecoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var preferenceDb = await SharedPreferences.getInstance();
+    await preferenceDb.setString('data', jsonEncode(widget.items));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-       
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: TextFormField(
+          controller: newTaskController,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(color: Colors.white),
+          ),
         ),
       ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                  save();
+                });
+              },
+            ),
+            key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+              child: Center(
+                child: Text("Removing"),
+              ),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: add,
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
